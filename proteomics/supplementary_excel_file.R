b991110710f35a9e29a4b2f8267342969e9c8e4a
Kacper Kaszuba <kacper.kaszuba0608@gmail.com>
2025-02-14 15:57:03 +0100

    Update supplementary_excel_file.R

--- proteomics/supplementary_excel_file.R
+++ proteomics/supplementary_excel_file.R
@@ -2,13 +2,24 @@
 # 1. Two files after log2 fold change calculation (MITOS and TOTALS)
 # 2. raw data (proteinGroups.txt)
 # 3. Results of GO enrichment (4 files: 2 up and 2 down)
+# 4. Organelles comparison
 
 # OUTPUT: supplementary file in excel format
 
 library(dplyr)
 
-fc_output_M <- readr::read_csv('./data/cleaned/OCIAD1_proteomics_mitos_process.csv')
-fc_output_T <- readr::read_csv('./data/cleaned/OCIAD1_proteomics_totals_process.csv')
+organelles <- readr::read_csv('./data/organelles.csv')
+
+fc_output_M <- readr::read_csv('./data/cleaned/OCIAD1_proteomics_mitos_process.csv') |> 
+    select(-contains('MitoCarta'))
+fc_output_M <- merge(fc_output_M, organelles, by=c('Protein IDs', 'Gene names'), all.x = TRUE) |>
+    distinct()
+
+fc_output_T <- readr::read_csv('./data/cleaned/OCIAD1_proteomics_totals_process.csv') |> 
+    select(-contains('MitoCarta'))
+fc_output_T <- merge(fc_output_T, organelles, by=c('Protein IDs', 'Gene names'), all.x = TRUE) |>
+    distinct()
+
 raw_data <- readr::read_tsv('./data/proteinGroups.txt') |>
     select(`Protein IDs`, `Gene names`, contains('LFQ intensity') 
            & (ends_with('22') | ends_with('23') | ends_with('24')))
@@ -29,20 +40,21 @@ df_go_totals_down <- readr::read_csv("./data/GO_enrichment/OCIAD1_proteomics_tot
     select(ONTOLOGY, ID, Description, GeneRatio, BgRatio, pvalue, 
            p.adjust, qvalue, geneID, Count)
 
-GO_vs_peroxisomeDB <- readr::read_csv('./GO_vs_peroxisomeDB.csv', show_col_types = F)
-
-peroxisomeDB_vs_GO <- readr::read_csv('./peroxisomeDB_vs_GO.csv', show_col_types = F)
-
 #### saving results ####
 readme <- data.frame("COLUMN NAME" = c('Protein IDs',
                                        'Gene names',
                                        'Protein names',
-                                       'MitoCarta3.0_SubMitoLocalization',
-                                       'MitoCarta3.0_MitoPathways',
                                        'FC_MITOS/TOTALS_OCIAD1',
                                        'p_OCIAD1_MITOS/TOTALS',
                                        'sig_MITOS/TOTALS_OCIAD1',
                                        'LFQ_WT(KO)_MITOS(TOTALS)_(22,23,24)',
+                                       'missingness',
+                                       'MitoCarta3.0',
+                                       'Peroxisome DB',
+                                       'One GO Mitochondrion',
+                                       'One GO Peroxisome',
+                                       'One GO ER',
+                                       '###################',
                                        'Ontology',
                                        'ID',
                                        'Description',
@@ -52,18 +64,21 @@ readme <- data.frame("COLUMN NAME" = c('Protein IDs',
                                        'p.adjust',
                                        'qvalue',
                                        'geneID',
-                                       'Count',
-                                       'GO term',
-                                       'PeroxisomeDB'),
+                                       'Count'),
                      "DESCRIPTION" = c('Identifiers of proteins contained in the protein group.',
-                                       'Names of genes this peptide is associated with.',
-                                       'Names of proteins this peptide is associated with.',
-                                       'Annotation to most likely compartment: matrix, inner membrane (MIM), intermembrane space (IMS), outer membrane (MOM), mitochondrial membrane, or unknown.',
-                                       'Annotation to a hierarchy of 149 biological pathways, based on literature.',
+                                       'Names of genes this protein is associated with.',
+                                       'Names of proteins this protein is associated with.',
                                        'Log2 Fold Change based on means of normalized LFQ intenisties.',
                                        'p-value from t-test.',
-                                       'Siginificance of peptide (p < 0.05 & log2(FC) > 1).',
-                                       'Imputed and normalized LFQ intensity values for peptides.',
+                                       'Siginificance of protein (p < 0.05 & log2(FC) > 1).',
+                                       'Imputed and normalized LFQ intensity values for proteins.',
+                                       'Type of missigness: compelete (complete), missing at random (MAR), missing nor at random (MNAR), worse p-value (NA)',
+                                       'Logical column containing if protein is in MitoCarta3.0 data base (TRUE/FALSE)',
+                                       'Assigned organelle name based on the PeroxisomeDB. (TRUE/FALSE)',
+                                       'Assigned mitochondrion using gene onthology for mitochondrion GO ID (TRUE/FALSE)',
+                                       'Assigned mitochondrion using gene onthology for peroxisome GO ID (TRUE/FALSE)',
+                                       'Assigned mitochondrion using gene onthology for endoplasmatic reticulum GO ID (TRUE/FALSE)',
+                                       '###################',
                                        'Category such as BP (biological process), CC (cellular component), MF (molecular function).',
                                        'Unique GO identifier.',
                                        'Description of the GO term.',
@@ -73,9 +88,8 @@ readme <- data.frame("COLUMN NAME" = c('Protein IDs',
                                        'The corrected p-value.',
                                        'The p-value corrected using the False Discovery Rate approach.',
                                        'Specific genes in the input dataset associated with the term.',
-                                       'The number of the genes in the input dataset associated with the term.',
-                                       'Assigned organelle name based on the GO terms.',
-                                       'Assigned organelle name based on the PeroxisomeDB.'))
+                                       'The number of the genes in the input dataset associated with the term.')
+                     )
 
 list_of_sheets <- list("README" = readme,
                        "OCIAD1_prot_mitos_process" = fc_output_M,
@@ -84,8 +98,6 @@ list_of_sheets <- list("README" = readme,
                        "OCIAD1_prot_mitos_GOup" = df_go_mitos_down,
                        "OCIAD1_prot_totals_GOdown" = df_go_totals_up,
                        "OCIAD1_prot_totals_GOup" = df_go_totals_down,
-                       "GO_vs_peroxisomeDB" = GO_vs_peroxisomeDB,
-                       "peroxisomeDB_vs_GO" = peroxisomeDB_vs_GO,
                        "OCIAD1_prot_raw_data" = raw_data)
 
 openxlsx::write.xlsx(list_of_sheets, file = "./supplementary_files/SuppFile2_Proteomics.xlsx",
